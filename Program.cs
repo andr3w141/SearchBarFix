ef574aa7d5cfd6872d11e66f97e6e2a74af7d131
AnDr3W <AnDr3W@DESKTOP-19238>
2024-01-15 21:51:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace SearchBarFix
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+
+            Console.WriteLine("Opening ctfmon...");
+            #region Start the ctfmon for Windows Explorer
+
+            Utils ctfmon = new Utils();
+            ctfmon.Path = @"C:\Windows\System32\ctfmon.exe";
+            ctfmon.CurrentProcess.StartInfo.FileName = ctfmon.Path;
+            ctfmon.CurrentProcess.StartInfo.UseShellExecute = true;
+            ctfmon.CurrentProcess.StartInfo.Verb = "runas";
+            ctfmon.CurrentProcess.Start();
+
+            #endregion
+
+            Console.WriteLine("Restarting explorer...");
+            #region Restart Windows Explorer for search bar
+
+            Utils explorer = new Utils();
+            string explorer_process_name = "explorer";
+            explorer.Path = @"C:\Windows\explorer.exe";
+            explorer.CurrentProcess = explorer.GetProcess(explorer_process_name);
+            explorer.RestartProcess(explorer.CurrentProcess);
+            #endregion
+
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SearchBarFix
+{
+    internal class Utils
+    {
+        private string path;
+        private Process process;
+
+        public string Path { get { return path; } set { path = value; } }
+
+        public Process CurrentProcess { get { return process; } set { process = value; } }
+        public Utils() 
+        {
+            Path = @"";
+            CurrentProcess = new Process();
+        }
+
+        public void StartProcess(string path)
+        {
+            Process.Start(path);
+        }
+
+        public void RestartProcess(Process process)
+        {
+            Thread.Sleep(1000);
+            process.Kill();
+            process.WaitForExit();
+            Process.Start(path);
+        }
+
+        public Process GetProcess(string name) 
+        {
+            return Process.GetProcessesByName(name).FirstOrDefault();
+        }
+    }
+}
